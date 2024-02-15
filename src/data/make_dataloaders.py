95b89ea9d8b85e3f76516491a80207540addb0f3
Jamie <jamienortonschool@gmail.com>
2024-02-15 17:53:36 +0000

    seperating functions into different files

---
+++ src/data/make_dataloaders.py
@@ -0,0 +1,23 @@
+
+import torch
+import numpy as np
+
+import torchvision
+from torchvision import models
+
+
+def img_loader(path):
+    image = np.asarray((io.imread(path))/32000,dtype='float32')
+    return image.transpose(2,0,1)
+
+def make_dataloaders(loader_func = img_loader):
+    
+    batch_size_val = 128
+    
+    data = torchvision.datasets.DatasetFolder(root=root,loader = loader_func, transform=None, extensions = 'jpg')
+    train_set, val_set = train_test_split(data, test_size = 0.2, stratify = data.targets)
+    
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_val, shuffle=True, num_workers=3, drop_last = True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size_val, shuffle=True, num_workers=0, drop_last = True)
+
+    return train_loader, val_loader
